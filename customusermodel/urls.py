586c7d919a76053a8becb925cb5bb49584bfae38
MICLEM <miclemabasie3@gmail.com>
2022-07-07 22:31:21 +0100

    configured basic usage of djoser

--- customusermodel/urls.py
+++ customusermodel/urls.py
@@ -1,21 +1,7 @@
-"""customusermodel URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+import djoser
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -24,8 +10,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("api/v1/profiles/", include("profiles.urls", namespace="profiles")),
-    path("api/v1/auth/", views.obtain_auth_token),
+    path("api/v1/auth/", include("djoser.urls")),
     path("admin/", admin.site.urls),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/authbasic/", views.obtain_auth_token),
 ]
